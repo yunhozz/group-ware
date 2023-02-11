87b7bfeb9f251a4d0dbeea68c5ba95c0a217332c
yunhozz <qkrdbsgh1121@naver.com>
2023-02-11 11:32:39 +0900

    feat: api 컨트롤러 추가, 관련 dto 추가

---
+++ auth-server/src/main/java/com/authserver/interfaces/AuthController.java
@@ -0,0 +1,68 @@
+package com.authserver.interfaces;
+
+import com.authserver.application.AuthService;
+import com.authserver.dto.request.JoinRequestDto;
+import com.authserver.dto.request.LoginRequestDto;
+import com.authserver.dto.response.TokenResponseDto;
+import com.authserver.dto.response.UserDataResponseDto;
+import com.authserver.dto.response.UserProfileResponseDto;
+import com.authserver.persistence.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDataResponseDto>> getUserListInfo() {
+        List<UserDataResponseDto> userDataResponseDtoList = userRepository.findUserList();
+        return ResponseEntity.ok(userDataResponseDtoList);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UserProfileResponseDto> getUserProfile(@PathVariable String userId) {
+        UserProfileResponseDto userProfileResponseDto = authService.findUserInfoByUserId(userId);
+        return ResponseEntity.ok(userProfileResponseDto);
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<String> join(@RequestBody JoinRequestDto joinRequestDto) {
+        String userId = authService.join(joinRequestDto);
+        return new ResponseEntity<>(userId, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        TokenResponseDto tokenResponseDto = authService.login(loginRequestDto, response);
+        return new ResponseEntity<>(tokenResponseDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String token) {
+        authService.logout(token);
+        return new ResponseEntity<>("로그아웃이 완료되었습니다.", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenResponseDto> tokenReissue(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String refreshToken, HttpServletResponse response) {
+        TokenResponseDto tokenResponseDto = authService.tokenReissue(refreshToken, response);
+        return new ResponseEntity<>(tokenResponseDto, HttpStatus.CREATED);
+    }
+}
