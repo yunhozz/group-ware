0823b8837c0489b343a5880c465bb6ac30c25dc0
yunhozz <qkrdbsgh1121@naver.com>
2023-02-11 11:25:54 +0900

    feat: jwt 생성, 필터, 핸들러에 관한 클래스 추가

---
+++ auth-server/src/main/java/com/authserver/dto/response/TokenResponseDto.java
@@ -0,0 +1,18 @@
+package com.authserver.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponseDto {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long refreshTokenValidTime;
+}
