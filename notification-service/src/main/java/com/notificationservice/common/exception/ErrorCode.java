118ea31ca992be9aeafec0f82b0162b6092a7df1
yunhozz <qkrdbsgh1121@naver.com>
2023-02-13 18:37:06 +0900

    feat: api exception handler 추가, 관련 파일 추가/수정
    - ErrorResponseDto, NotValidResponseDto
    - ErrorCode 에러 유형 추가

--- notification-service/src/main/java/com/notificationservice/common/exception/ErrorCode.java
+++ notification-service/src/main/java/com/notificationservice/common/exception/ErrorCode.java
@@ -7,7 +7,9 @@ public enum ErrorCode {
 
     INTER_SERVER_ERROR(500, "N-001", "서버 에러가 발생했습니다."),
     NOTIFICATION_NOT_FOUND(404, "N-002", "해당 알림을 찾을 수 없습니다."),
-    DATA_SEND_FAIL(400, "N-003", "데이터 전송에 실패하였습니다.")
+    DATA_SEND_FAIL(400, "N-003", "데이터 전송에 실패하였습니다."),
+    NOT_AUTHORIZED(401, "N-004", "해당 리소스에 대한 접근 권한이 없습니다."),
+    NOT_VALID(400, "N-005", "잘못된 요청입니다.")
 
     ;
 
