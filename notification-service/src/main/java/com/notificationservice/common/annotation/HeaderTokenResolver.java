179800db3edd5a388754ef81755e039943061b9a
yunhozz <qkrdbsgh1121@naver.com>
2023-02-13 18:38:51 +0900

    feat: 헤더 토큰 어노테이션 추가, 관련 클래스 추가
    - HeaderTokenResolver
    - resolver 등록 (WebMvcConfig)
    - TokenParser 추가

---
+++ notification-service/src/main/java/com/notificationservice/common/annotation/HeaderTokenResolver.java
@@ -0,0 +1,56 @@
+package com.notificationservice.common.annotation;
+
+import com.notificationservice.application.exception.NotAuthorizedException;
+import com.notificationservice.common.enums.Role;
+import com.notificationservice.common.util.TokenParser;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HeaderTokenResolver implements HandlerMethodArgumentResolver {
+
+    private final TokenParser tokenParser;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasHeaderTokenAnnotation = parameter.hasParameterAnnotation(HeaderToken.class);
+        boolean isStringType = String.class.isAssignableFrom(parameter.getParameterType());
+        return hasHeaderTokenAnnotation && isStringType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String auth = (String) tokenParser.execute(token).get("auth");
+
+        HeaderToken headerToken = parameter.getParameterAnnotation(HeaderToken.class);
+        Role[] roles = headerToken.role();
+
+        if (!isAuthorized(roles, auth)) {
+            throw new NotAuthorizedException();
+        }
+
+        return token;
+    }
+
+    private boolean isAuthorized(Role[] roles, String auth) {
+        for (Role role : roles) {
+            if (role.getAuth().equals(auth)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
