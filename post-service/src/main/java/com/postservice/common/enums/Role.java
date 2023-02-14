de186bbafe74122ba968ecbde1c4a75317fa1280
yunhozz <qkrdbsgh1121@naver.com>
2023-02-14 19:17:20 +0900

    feat: token 파싱 관련 클래스 추가
    - HeaderToken annotation, Resolver
    - enum Role
    - TokenParser

---
+++ post-service/src/main/java/com/postservice/common/enums/Role.java
@@ -0,0 +1,19 @@
+package com.postservice.common.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER"),
+    GUEST("ROLE_GUEST")
+
+    ;
+
+    private final String auth;
+
+    Role(String auth) {
+        this.auth = auth;
+    }
+}
