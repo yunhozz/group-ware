d69daa5c12c13655d94ec39bcdd95dea4ac9fac2
yunhozz <qkrdbsgh1121@naver.com>
2023-02-14 12:11:22 +0900

    feat: HttpClientErrorException 에 대한 핸들러 추가

--- notification-service/src/main/java/com/notificationservice/common/exception/NotificationExceptionHandler.java
+++ notification-service/src/main/java/com/notificationservice/common/exception/NotificationExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.notificationservice.common.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.notificationservice.common.exception.dto.ErrorResponseDto;
 import com.notificationservice.common.exception.dto.NotValidResponseDto;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +12,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +47,15 @@ public class NotificationExceptionHandler {
         }};
 
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.NOT_VALID, notValidResponseDtoList);
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ErrorResponseDto> handleHttpClientErrorException(HttpClientErrorException e) throws JsonProcessingException {
+        log.error("handleHttpClientErrorException", e);
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule()); // LocalDateTime 변환
+        ErrorResponseDto errorResponseDto = objectMapper.readValue(e.getResponseBodyAsString(), ErrorResponseDto.class);
+
         return new ResponseEntity<>(errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 }
