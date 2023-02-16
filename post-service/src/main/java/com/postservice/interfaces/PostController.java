6e51f3119d7aa29d2da4af94046ebb1b38f7966b
yunhozz <qkrdbsgh1121@naver.com>
2023-02-16 18:03:17 +0900

    fix: 게시물 생성, 수정 api 의 메소드 변수 수정

--- post-service/src/main/java/com/postservice/interfaces/PostController.java
+++ post-service/src/main/java/com/postservice/interfaces/PostController.java
@@ -20,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -28,8 +29,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -103,7 +106,7 @@ public class PostController {
         return ResponseEntity.ok(postSimpleDtoList);
     }
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<Slice<PostSimpleQueryDto>> getSimplePostSliceByType(@RequestParam PostType postType, @RequestParam(required = false) Long cursorId, Pageable pageable) {
         Slice<PostSimpleQueryDto> postSimpleDtoSlice = postRepository.getPostSimpleSliceByType(postType, cursorId, pageable);
         if (!postSimpleDtoSlice.isEmpty()) {
@@ -125,19 +128,23 @@ public class PostController {
         return ResponseEntity.ok(postSimpleDtoSlice);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Long> createPost(@HeaderToken(role = {Role.ADMIN, Role.USER}) String token, @RequestParam(required = false) Long teamId,
-                                           @Valid @RequestBody PostRequestDto postRequestDto) {
+    @PostMapping(value = "/create", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Long> createPost(@HeaderToken(role = {Role.ADMIN, Role.USER}) String token,
+                                           @RequestParam(required = false) Long teamId,
+                                           @Valid @RequestBody PostRequestDto postRequestDto,
+                                           @RequestPart(name = "files", required = false) List<MultipartFile> files) {
         Claims claims = tokenParser.execute(token);
-        Long postId = postService.createPost(claims.getSubject(), teamId, postRequestDto);
+        Long postId = postService.createPost(claims.getSubject(), teamId, postRequestDto, files);
         return ResponseEntity.ok(postId);
     }
 
-    @PatchMapping("/{id}/update")
-    public ResponseEntity<Long> updatePost(@HeaderToken(role = {Role.ADMIN, Role.USER}) String token, @PathVariable Long id,
-                                           @Valid @RequestBody PostUpdateRequestDto postUpdateRequestDto) {
+    @PatchMapping(value = "/{id}/update", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Long> updatePost(@HeaderToken(role = {Role.ADMIN, Role.USER}) String token,
+                                           @PathVariable Long id,
+                                           @Valid @RequestBody PostUpdateRequestDto postUpdateRequestDto,
+                                           @RequestPart(name = "files", required = false) List<MultipartFile> files) {
         Claims claims = tokenParser.execute(token);
-        Long postId = postService.updateInfo(id, claims.getSubject(), postUpdateRequestDto);
+        Long postId = postService.updateInfo(id, claims.getSubject(), postUpdateRequestDto, files);
         return new ResponseEntity<>(postId, HttpStatus.CREATED);
     }
 
