86d505dbd5ff1348047cf6942b80ea557e925145
yunhozz <qkrdbsgh1121@naver.com>
2023-03-06 19:00:37 +0900

    feat: 만료된 메일 리스트 soft delete 쿼리 생성

--- mail-service/src/main/java/com/mailservice/persistence/repository/MailRepository.java
+++ mail-service/src/main/java/com/mailservice/persistence/repository/MailRepository.java
@@ -2,6 +2,24 @@ package com.mailservice.persistence.repository;
 
 import com.mailservice.persistence.entity.mail.Mail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MailRepository<T extends Mail> extends JpaRepository<T, Long>, MailRepositoryCustom {
+
+    @Query("select m.id from MailWrite mw join mw.mail m join mw.mailSecurity ms where ms.validity is not null and ms.validity >= :now")
+    List<Long> findIdsAfterNow(@Param("now") LocalDateTime now);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Mail m set m.isDeleted = true where m.id in :ids")
+    void deleteListInIds(@Param("ids") List<Long> ids);
+
+    default void deleteExpiredMailList(LocalDateTime now) {
+        List<Long> ids = findIdsAfterNow(now);
+        deleteListInIds(ids);
+    }
 }
